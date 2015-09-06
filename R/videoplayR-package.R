9ac4161d887d6e99f70b1ccf071fd31890232571
Simon Garnier <simon.garnier@gmail.com>
2015-09-06 14:06:45 -0400

    Updated package documentation.

---
+++ R/videoplayR-package.R
@@ -0,0 +1,24 @@
+#' videoplayR: A basic (for now) computer vision library for R.
+#'
+#' The \code{videoplayR} package uses the OpenCV library to provide R users with 
+#' some basic functions to read and manipulate image and video files. 
+#' 
+#' @author Simon Garnier <garnier@@njit.edu> - \href{http://twitter.com/sjmgarnier}{@@sjmgarnier}
+#' 
+#' Maintainer: Simon Garnier <garnier@@njit.edu> - \href{http://twitter.com/sjmgarnier}{@@sjmgarnier}
+#' 
+#' @details 
+#' \tabular{ll}{
+#'  Package: \tab videoplayR\cr
+#'  Type: \tab Package\cr
+#'  Version: \tab 0.3\cr
+#'  Date: \tab 2015-09-04\cr
+#'  License: \tab GPL-3\cr
+#' }
+#'
+#' @references More about OpenCV at \link{http://opencv.org/}.
+#'
+#' @docType package
+#' @name videoplayR
+NULL
+#> NULL
